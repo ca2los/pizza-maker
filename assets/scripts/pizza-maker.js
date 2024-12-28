b2b40e260348ac89736b85fd73054749e85e976f
ca2los <6784754+ca2los@users.noreply.github.com>
2024-12-27 22:36:41 -0600

    OnClick tests to retrieve data.

--- assets/scripts/pizza-maker.js
+++ assets/scripts/pizza-maker.js
@@ -1,15 +1,18 @@
-// If name and phone exist, then proceeds with the pizza maker.
+// If name and phone exist, then proceeds with the pizza maker
+// Reset button to start again the process
 
 function checkName(reviewName) {
     console.log(reviewName);
+    alert(reviewName)
 }
 function checkTel(reviewTel) {
     console.log(reviewTel);
+    alert(reviewTel);
 }
 
 function pizzaCustomer() {
-    let nameSubmit = document.getElementById("pm-name");
-    let phoneSubmit = document.getElementById("pm-tel");
+    let nameSubmit = document.getElementById("pm-name").value;
+    let phoneSubmit = document.getElementById("pm-tel").value;
     checkName(nameSubmit)
     checkTel(phoneSubmit);
 }
