fb520c3676818db4b289afd9be9bfb0d406d2d77
ca2los <6784754+ca2los@users.noreply.github.com>
2024-12-28 23:43:32 -0600

    OnClick tests to retrieve data.

--- assets/scripts/pizza-maker.js
+++ assets/scripts/pizza-maker.js
@@ -4,10 +4,11 @@
 
 function checkUsername (reviewName, reviewTel) {
     let node1 = document.createElement('h3')
-    let node2 = document.createElement('p')
+    let node2 = document.createElement('h4')
     let node3 = document.createElement('p')
     let node4 = document.createElement('button')
     let node5 = document.createElement('button')
+    document.getElementById('pm-output').style.display = 'block'
     document.getElementById('pm-output').appendChild(node1).textContent = reviewName
     document.getElementById('pm-output').appendChild(node2).textContent = reviewTel
     document.getElementById('pm-output').appendChild(node3).textContent = "Verify the data and click 'Check' to continue or 'Cancel' to restart."
