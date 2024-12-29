87f600671937e70124d8c5fa282b1790c4ac1663
ca2los <6784754+ca2los@users.noreply.github.com>
2024-12-28 23:18:27 -0600

    OnClick tests to retrieve data.

--- assets/scripts/pizza-maker.js
+++ assets/scripts/pizza-maker.js
@@ -1,19 +1,22 @@
+"use strict";
 // If name and phone exist, then proceeds with the pizza maker
 // Reset button to start again the process
 
-function checkName(reviewName) {
-    console.log(reviewName);
-    alert(reviewName)
-}
-function checkTel(reviewTel) {
-    console.log(reviewTel);
-    alert(reviewTel);
+function checkUsername (reviewName, reviewTel) {
+    let node1 = document.createElement('h3')
+    let node2 = document.createElement('p')
+    let node3 = document.createElement('p')
+    let node4 = document.createElement('button')
+    let node5 = document.createElement('button')
+    document.getElementById('pm-output').appendChild(node1).textContent = reviewName
+    document.getElementById('pm-output').appendChild(node2).textContent = reviewTel
+    document.getElementById('pm-output').appendChild(node3).textContent = "Verify the data and click 'Check' to continue or 'Cancel' to restart."
+    document.getElementById('pm-output').appendChild(node4).setAttribute('type','reset')
+    document.getElementById('pm-output').appendChild(node5).setAttribute('type','button')
 }
 
 function pizzaCustomer() {
     let nameSubmit = document.getElementById("pm-name").value;
     let phoneSubmit = document.getElementById("pm-tel").value;
-    checkName(nameSubmit)
-    checkTel(phoneSubmit);
+    checkUsername(nameSubmit, phoneSubmit);
 }
-
