100dfa9545e060b53180de3742268de127bfdb6e
ca2los <6784754+ca2los@users.noreply.github.com>
2024-12-29 21:22:19 -0600

    Assigning "onClick" actions in buttons.

--- assets/scripts/pizza-maker.js
+++ assets/scripts/pizza-maker.js
@@ -2,18 +2,26 @@
 // If name and phone exist, then proceeds with the pizza maker
 // Reset button to start again the process
 
-function checkUsername (reviewName, reviewTel) {
+function pizzaMenu() {
+
+}
+
+function checkUsername(reviewName, reviewTel) {
     let node1 = document.createElement('h3')
     let node2 = document.createElement('h4')
     let node3 = document.createElement('p')
     let node4 = document.createElement('button')
     let node5 = document.createElement('button')
+
     document.getElementById('pm-output').style.display = 'block'
     document.getElementById('pm-output').appendChild(node1).textContent = reviewName
     document.getElementById('pm-output').appendChild(node2).textContent = reviewTel
-    document.getElementById('pm-output').appendChild(node3).textContent = "Verify the data and click 'Check' to continue or 'Cancel' to restart."
+    document.getElementById('pm-output').appendChild(node3).textContent = "Do you want to proceed?"
     document.getElementById('pm-output').appendChild(node4).setAttribute('type','reset')
     document.getElementById('pm-output').appendChild(node5).setAttribute('type','button')
+
+    document.querySelector('button[type="reset"]').onclick = function(){location.reload();}
+    document.querySelector('button[type="submit"]').onclick = pizzaMenu()
 }
 
 function pizzaCustomer() {
