ffbe0fc09e7a0180929f1a924ef38f5288c25316
ca2los <6784754+ca2los@users.noreply.github.com>
2025-01-01 22:25:45 -0600

    Working on pizzaMenu()

--- assets/scripts/pizza-maker.js
+++ assets/scripts/pizza-maker.js
@@ -2,33 +2,34 @@
 // If name and phone exist, then proceeds with the pizza maker
 // Reset button to start again the process
 
+const HTML_ITEMS = [
+    document.createElement('h2'),
+    document.createElement('h3'),
+    document.createElement('h4'),
+    document.createElement('p'),
+    document.createElement('button'),
+    document.createElement('button')
+]
+
 function pizzaMenu() {
-    // Select Pizza
-    let node1 = document.createElement('h2')
     // Hide & Display
     document.getElementById('pizza-form').style.display = 'none'
     document.getElementById('pm-menu').style.display = 'block'
     // Append Objects
-    document.getElementById('pm-menu').appendChild(node1).textContent = "Select your pizza"
+    document.getElementById('pm-menu').appendChild(HTML_ITEMS[0]).textContent = "Select your pizza"
 }
 
 function checkUsername(reviewName, reviewTel) {
-    // Create Objects
-    let node1 = document.createElement('h3')
-    let node2 = document.createElement('h4')
-    let node3 = document.createElement('p')
-    let node4 = document.createElement('button')
-    let node5 = document.createElement('button')
     // Display Object
     document.getElementById('pm-output').style.display = 'block'
     // Append Objects
-    document.getElementById('pm-output').appendChild(node1).textContent = reviewName
-    document.getElementById('pm-output').appendChild(node2).textContent = reviewTel
-    document.getElementById('pm-output').appendChild(node3).textContent = "Do you want to proceed?"
-    document.getElementById('pm-output').appendChild(node4).setAttribute('type','reset')
-    document.getElementById('pm-output').appendChild(node5).setAttribute('type','button')
+    document.getElementById('pm-output').appendChild(HTML_ITEMS[1]).textContent = reviewName
+    document.getElementById('pm-output').appendChild(HTML_ITEMS[2]).textContent = reviewTel
+    document.getElementById('pm-output').appendChild(HTML_ITEMS[3]).textContent = "Do you want to proceed?"
+    document.getElementById('pm-output').appendChild(HTML_ITEMS[4]).setAttribute('type','reset')
+    document.getElementById('pm-output').appendChild(HTML_ITEMS[5]).setAttribute('type','button')
     // Listen OnClick
-    document.querySelector('button[type="reset"]').onclick = function(){location.reload();}
+    document.querySelector('button[type="reset"]').onclick = function(){location.reload()}
     document.querySelector('button[type="button"]').addEventListener('click', pizzaMenu)
 }
 
@@ -40,5 +41,4 @@ function pizzaCustomer() {
 }
 
 // ToDo
-// Append createElements into an object
-// Make pizzaMenu work
+// Make pizzaMenu() work
